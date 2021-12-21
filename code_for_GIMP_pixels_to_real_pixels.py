262ef6ffc8d4dcd76bf0e3e545ce9f1998a9b612
Frank Yu <frank@yuyuan.org>
2021-12-20 22:52:57 -0800

    Update!

---
+++ code_for_GIMP_pixels_to_real_pixels.py
@@ -0,0 +1,21 @@
+for u in range(1, 3):
+	v = 20
+	x = 0
+	y = 0
+	z = 1
+	
+	for w in range(1, v + 1):
+		x += 1
+		y += 1
+	
+		if y == 3 or w == 2:
+			z += 1
+			y = 0
+	
+		if u == 1:
+			print(f"{x} = {z}")
+		else:
+			print(f"{x}:{z}")
+        
+	if u == 1:
+		print("\n \n \n \n")
